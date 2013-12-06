8ca7fb6b38cb4d49e8a058703a1a7ee69eac410a
Sol <SolArgons@gmail.com>
2013-12-06 13:11:03 +0800

    add new file:   Reverse_Integer.cpp

---
+++ Reverse_Integer.cpp
@@ -0,0 +1,21 @@
+/**
+* Reverse digits of an integer.
+* Example1: x = 123, return 321
+* Example2: x = -123, return -321
+*/
+
+class Solution {
+    public:
+    int reverse(int x) {
+        int result = 0;
+        int n = abs(x);
+        while (n) {
+            result += result * 10 + n % 10;
+            n /= 10;
+        }
+        if (x < 0) {
+            return -result;
+        }
+        return result;
+    }
+};
