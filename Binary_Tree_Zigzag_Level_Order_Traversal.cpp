3f4ec21b82067f5cc8053ad9f891e8f34be2d6dd
Sol <SolArgons@gmail.com>
2014-01-30 01:16:59 +0800

    Binary_Tree_Zigzag_Level_Order_Traversal

--- Binary_Tree_Zigzag_Level_Order_Traversal.cpp
+++ Binary_Tree_Zigzag_Level_Order_Traversal.cpp
@@ -16,4 +16,44 @@
 //  [15,7]
 // ]
 
+class Solution {
+public:
+    vector<vector<int> > zigzagLevelOrder(TreeNode *root) {
+        vector<vector<int> > result;
+        vector<int> level;
+        if (root == NULL)   
+            return result;
 
+        queue<TreeNode*> nodes;
+        nodes.push(root);
+        nodes.push(NULL);
+
+        while (!nodes.empty()) {
+            TreeNode *current = nodes.front();
+            nodes.pop();
+
+            if (current == NULL) {
+                result.push_back(level);
+                level.clear();
+                if (nodes.empty())
+                    break;
+                nodes.push(NULL);
+            } 
+            else {
+                level.push_back(current->val);
+                if (current->left != NULL)
+                    nodes.push(current->left);
+                if (current->right != NULL)
+                    nodes.push(current->right);
+            }
+        }
+
+        // reverse the order of odd index level vectors
+        for (int i = 1; i < result.size(); i += 2) {
+            vector<int> temp(result[i]);
+            result[i].clear();
+            result[i].insert(result[i].end(), temp.rbegin(), temp.rend());
+        }
+        return result;
+    }
+};
