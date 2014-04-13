76e46664985afd37077486bf2f5310ebd0b85b92
Sol <SolArgons@gmail.com>
2014-04-13 15:23:38 +0800

    leftover

--- Longest_Valid_Parentheses.cpp
+++ Longest_Valid_Parentheses.cpp
@@ -6,4 +6,23 @@
 // Another example is ")()())", where the longest valid parentheses substring 
 // is "()()", which has length = 4.
 
-
+class Solution {
+public:
+    int longestValidParentheses(const string &s) {
+        int maxLen = 0;
+        vector<int> dp(s.length(), 0);
+        for (int i = s.size()-2; i >= 0; i--) {
+            if (s[i] == '(') {
+                int j = i+1 + dp[i+1];
+                if (j < s.size() && s[j] == ')') {
+                    dp[i] = dp[i+1] + 2; // "(())j"
+                    if (j + 1 < s.size()) {
+                        dp[i] += dp[j+1]; // "()j(())"
+                    }
+                }
+            }
+            maxLen = max(maxLen, dp[i]);
+        }
+        return maxLen;
+    }
+};
